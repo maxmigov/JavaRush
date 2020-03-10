7da68939cceb8b6b2861a7a1960698d7dc4cc9fd
maxmigov <60349576+maxmigov@users.noreply.github.com>
2020-03-10 04:32:46 +0200

    Три метода и минимум
    Три метода и минимум

---
+++ JavaCore/src/com/javarush/task/task12/task1209/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task12.task1209;
+
+/* 
+Три метода и минимум
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    static int min(int a, int b){
+        if (a > b){
+            return b;
+        }
+        else
+            return a;
+    }
+    static long min(long a, long b){
+        if (a > b){
+            return b;
+        }
+        else
+            return a;
+    }
+    static double min(double a, double b){
+        if (a > b){
+            return b;
+        }
+        else
+            return a;
+    }
+}
