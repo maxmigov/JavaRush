0741106e50f645cf8fd19c0454975804e79b85ba
maxmigov <60349576+maxmigov@users.noreply.github.com>
2020-03-10 04:32:07 +0200

    Свобода печати
    Свобода печати

---
+++ JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    static void print(int a){
+        
+    }
+    static void print(double a){
+        
+    }
+    static void print(float a){
+        
+    }
+    static void print(String a){
+        
+    }
+    static void print(char a){
+        
+    }
+}
