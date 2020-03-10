3555d86d76572355fd42241b28a3330266eb58cb
maxmigov <60349576+maxmigov@users.noreply.github.com>
2020-03-10 04:33:22 +0200

    Три метода и максимум
    Три метода и максимум

---
+++ JavaCore/src/com/javarush/task/task12/task1210/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task12.task1210;
+
+/* 
+Три метода и максимум
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    static int max(int a, int b){
+        if (a > b){
+            return a;
+        }
+        else
+            return b;
+    }
+    static long max(long a, long b){
+        if (a > b){
+            return a;
+        }
+        else
+            return b;
+    }
+    static double max(double a, double b){
+        if (a > b){
+            return a;
+        }
+        else
+            return b;
+    }
+}
