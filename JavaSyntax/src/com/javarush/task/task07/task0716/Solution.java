963de16a0139723386b5af5297152dccd63c4186
maxmigov <60349576+maxmigov@users.noreply.github.com>
2020-03-21 02:16:38 +0200

    Р или Л
    Р или Л

---
+++ JavaSyntax/src/com/javarush/task/task07/task0716/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task07.task0716;
+
+import java.util.ArrayList;
+
+/* 
+Р или Л
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<String>();
+        strings.add("роза");
+        strings.add("лоза");
+        strings.add("лира");
+        strings.add("термины");
+        strings.add("материал");
+        strings.add("Алгоритм");
+        strings = fix(strings);
+
+        for (String string : strings) {
+            System.out.println(string);
+        }
+    }
+
+    public static ArrayList<String> fix(ArrayList<String> strings) {
+        for (int i = 0; i < strings.size(); i++){
+            String tmp = strings.get(i);
+            if (tmp.contains("р") && !tmp.contains("л")) {
+                strings.remove(i);
+                i--;
+            }
+            if (tmp.contains("л") && !tmp.contains("р")) {
+                strings.add(i, strings.get(i));
+                i++;
+            }
+        }
+        return strings;
+    }
+}
